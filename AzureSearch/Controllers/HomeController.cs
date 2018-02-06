a2289e204b608e1cba3981a3052a8542c4b75001
kacpersw <swislocki.kacper@gmail.com>
2018-02-06 21:46:53 +0100

    aa

---
+++ AzureSearch/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using AzureSearch.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzureSearch.Controllers
+{
+    public class HomeController : Controller
+    {
+        private HotelsSearch hotelsSearch;
+
+        public HomeController()
+        {
+            hotelsSearch = new HotelsSearch();
+        }
+
+        public ActionResult Index()
+        {
+            var response = hotelsSearch.Search("Luxury", "1", 0, "", "", "Fancy Stay", "Luxury", null, false, false, new DateTime(2010, 6, 27), 5);
+
+            var jsonResult = new JsonResult
+            {
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet,
+                Data = new NYCHotels() { Results = response.Results, Facets = response.Facets, Count = Convert.ToInt32(response.Count) }
+            };
+
+            return jsonResult;
+        }
+    }
+}
---
+++ AzureSearch/HotelsSearch.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace AzureSearch
+{
+    public class HotelsSearch
+    {
+        private readonly SearchServiceClient searchClient;
+        private readonly ISearchIndexClient indexClient;
+        private const string IndexName = "hotels";
+
+        public string errorMessage;
+
+        public HotelsSearch()
+        {
+            try
+            {
+                string searchServiceName = ConfigurationManager.AppSettings["SearchServiceName"];
+                string apiKey = ConfigurationManager.AppSettings["SearchServiceApiKey"];
+
+                searchClient = new SearchServiceClient(searchServiceName, new SearchCredentials(apiKey));
+                indexClient = searchClient.Indexes.GetClient(IndexName);
+            }
+            catch (Exception e)
+            {
+                errorMessage = e.Message.ToString();
+            }
+        }
+
+        public DocumentSearchResult Search(string searchText, string hotelId, double baseRate, string description, string description_fr, string hotelName, string category,
+            IEnumerable<string> tags, bool parkingIncluded, bool smokingAllowed, DateTime lastRenovationDate, int rating)
+        {
+            try
+            {
+                SearchParameters sp = new SearchParameters()
+                {
+                    SearchMode = SearchMode.Any,
+                    Top = 10,
+                    Select = new List<String> { "category" },
+                    Facets = new List<String> { "category" }
+                };
+
+                return indexClient.Documents.Search(searchText, sp);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error querying index: {0}\r\n", ex.Message.ToString());
+            }
+
+            return null;
+        }
+    }
+}
